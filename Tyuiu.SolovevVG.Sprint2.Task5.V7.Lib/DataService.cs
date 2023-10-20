daaef84e0bea3ac535f49d4afc05939d6561ef5b
SolovevVG <Tyuiu.SolovevVG@outlook.com>
2023-10-20 21:33:19 +0500

    Add Task5

---
+++ Tyuiu.SolovevVG.Sprint2.Task5.V7.Lib/DataService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+
+namespace Tyuiu.SolovevVG.Sprint2.Task5.V7.Lib
+{
+    public class DataService : ISprint2Task5V7
+    {
+        public string FindMonthName(int startYear, int n)
+        {
+            string res;
+            if (n > 12)
+            {
+                while (n > 12)
+                {
+                    n -= 12;
+                }
+            }
+            switch (n)
+            {
+                case 1:
+                    res = "Февраль";
+                    break;
+
+                case 2:
+                    res = "Март";
+                    break;
+
+                case 3:
+                    res = "Апрель";
+                    break;
+
+                case 4:
+                    res = "Май";
+                    break;
+
+                case 5:
+                    res = "Июнь";
+                    break;
+
+                case 6:
+                    res = "Июль";
+                    break;
+
+                case 7:
+                    res = "Август";
+                    break;
+
+                case 8:
+                    res = "Сентябрь";
+                    break;
+
+                case 9:
+                    res = "Октябрь";
+                    break;
+
+                case 10:
+                    res = "Ноябрь";
+                    break;
+
+                case 11:
+                    res = "Декабрь";
+                    break;
+
+                default:
+                    res = "Январь";
+                    break;
+            }
+
+            return res;
+        }
+
+    }
+}
---
+++ Tyuiu.SolovevVG.Sprint2.Task5.V7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SolovevVG.Sprint2.Task5.V7.Lib;
+
+namespace Tyuiu.SolovevVG.Sprint2.Task5.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Соловьев В.Г. | СМАРТБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Соловьев В.Г. | СМАРТБ-23-1                                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int y = 1990, n;
+
+            Console.WriteLine("Введите кол-во прошедших месяцев:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            var s = ds.FindMonthName(y, n);
+            Console.WriteLine("Сейчас " + s);
+            Console.ReadKey();
+        }
+    }
+}
